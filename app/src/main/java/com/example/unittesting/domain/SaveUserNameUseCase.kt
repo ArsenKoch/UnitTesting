6e25dff8dc4ca151fe05bef00e53c71d6b357f24
ArsenKoch <arsenkocaran@icloud.com>
2022-12-27 22:52:44 +0300

    Edit method in SaveUserNameUseCase.kt

--- app/src/main/java/com/example/unittesting/domain/SaveUserNameUseCase.kt
+++ app/src/main/java/com/example/unittesting/domain/SaveUserNameUseCase.kt
@@ -3,6 +3,12 @@ package com.example.unittesting.domain
 class SaveUserNameUseCase(private val userRepository: UserRepository) {
 
     fun execute(param: SaveUserNameParam): Boolean {
-        return userRepository.saveName(param)
+        val oldUserName = userRepository.getName()
+        if (oldUserName.firstName == param.name) {
+            return true
+        }
+
+        val result: Boolean = userRepository.saveName(saveParam = param)
+        return result
     }
 }
