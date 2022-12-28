568396f3212d088f8a7c000fc5aae7139220c711
ArsenKoch <arsenkocaran@icloud.com>
2022-12-28 17:51:50 +0300

    Create new MainViewModelTest.kt and write three test

---
+++ app/src/testDebug/java/com/example/unittesting/presentation/MainViewModelTest.kt
@@ -0,0 +1,33 @@
+package com.example.unittesting.presentation
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class MainViewModelTest {
+
+    @AfterEach
+    fun afterEach() {
+
+    }
+
+    @BeforeEach
+    fun beforeEach() {
+
+    }
+
+    @Test
+    fun shouldSaveUsernameReturnTrue() {
+
+    }
+
+    @Test
+    fun shouldSaveUsernameReturnFalse() {
+
+    }
+
+    @Test
+    fun shouldLoadData() {
+
+    }
+}
