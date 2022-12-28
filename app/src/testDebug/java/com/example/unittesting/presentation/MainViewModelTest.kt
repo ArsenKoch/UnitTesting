cc3230114ba6093236233df9a5f421f8e05000a0
ArsenKoch <arsenkocaran@icloud.com>
2022-12-28 17:54:31 +0300

    Use Mockito in MainViewModelTest.kt

--- app/src/testDebug/java/com/example/unittesting/presentation/MainViewModelTest.kt
+++ app/src/testDebug/java/com/example/unittesting/presentation/MainViewModelTest.kt
@@ -1,14 +1,22 @@
 package com.example.unittesting.presentation
 
+import com.example.unittesting.domain.GetUserNameUseCase
+import com.example.unittesting.domain.SaveUserNameUseCase
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.mockito.kotlin.mock
 
 class MainViewModelTest {
 
+    val getUserNameUseCase = mock<GetUserNameUseCase>()
+    val saveUserNameUseCase = mock<SaveUserNameUseCase>()
+
     @AfterEach
     fun afterEach() {
-
+        Mockito.reset(getUserNameUseCase)
+        Mockito.reset(saveUserNameUseCase)
     }
 
     @BeforeEach
